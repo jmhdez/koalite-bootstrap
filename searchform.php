bdfb2bc98b695f63d630e6bb74e54e65ce2a4dfa
jmhdez <juan.hernandez.arroyo@gmail.com>
2012-04-08 19:59:22 +0200

    removed search button in search widget

--- searchform.php
+++ searchform.php
@@ -1,10 +1,8 @@
-<form action="<?php echo home_url( '/' ); ?>" method="get" class="form-stacked">
+<form action="<?php echo home_url( '/' ); ?>" method="get" class="form-inline">
     <fieldset>
 		<div class="clearfix">
 			<div class="input-prepend">
-				<span class="add-on"><i class="icon-search"></i></span>
-				<input type="text" name="s" id="search" placeholder="<?php _e("Search","bonestheme"); ?>" value="<?php the_search_query(); ?>" />			
-				<button type="submit" class="btn btn-primary"><?php _e("Search","bonestheme"); ?></button>
+				<span class="add-on"><i class="icon-search"></i></span><input type="text" class="span3" name="s" id="search" placeholder="<?php _e("Search","bonestheme"); ?>" value="<?php the_search_query(); ?>" />		
 			</div>
         </div>
     </fieldset>
