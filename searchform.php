a6b8705973372fd37edf2b928e9103393a73b4da
jmhdez <juan.hernandez.arroyo@gmail.com>
2012-04-29 19:27:42 +0200

    Fixed searchform problems in show browsers (Chrome/Safari)

--- searchform.php
+++ searchform.php
@@ -1,9 +1,8 @@
-<form action="<?php echo home_url( '/' ); ?>" method="get" class="form-inline">
+<form action="<?php echo home_url( '/' ); ?>" method="get">
     <fieldset>
-		<div class="clearfix">
-			<div class="input-prepend">
-				<span class="add-on"><i class="icon-search"></i></span><input type="text" class="span2" name="s" id="search" placeholder="<?php _e("Search","bonestheme"); ?>" value="<?php the_search_query(); ?>" />		
-			</div>
-        </div>
+		<div class="input-prepend">
+			<span class="add-on"><i class="icon-search"></i></span><input type="text" class="span2" name="s" id="search" placeholder="<?php _e("Search","bonestheme"); ?>" value="<?php the_search_query(); ?>" />		
+		</div>
     </fieldset>
 </form>
+
