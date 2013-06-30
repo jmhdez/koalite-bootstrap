a270e37ea7a7f8904bc15fbad62e7e38cb546057
Juan M. Hernández <juan.hernandez.arroyo@gmail.com>
2013-06-30 11:13:14 +0200

    formatted archives page

--- archives.php
+++ archives.php
@@ -3,12 +3,6 @@
 <div id="archives">
 
 	<div class="row-fluid">
-		<div class="span12">
-			<?php get_search_form(); ?>
-		</div>
-	</div>
-
-	<div class="row-fluid">
 
 		<div class="span9">
 			<h3>Posts</h3>
@@ -26,19 +20,39 @@
 			</ul>
 		</div>
 
+
 		<div class="span3">
-			<h3>Etiquetas</h3>
-			<ul id="archives-tags">
-				<?php 
-					$tags = get_tags();
-					foreach ($tags as $tag) {
-						echo '<li><a href="' . get_tag_link($tag->term_id) . '" title="Posts sobre ' . $tag->name . '">' . $tag->name . '</a></li>' ;
-					}
-				?>
-			</ul>
+
+			<div class="row-fluid">
+				<h3>Buscar</h3>
+				<form action="<?php echo home_url( '/' ); ?>" method="get">
+					<div class="control-group">
+						<div class="controls">
+							<div class="input-prepend">
+								<span class="add-on"><i class="icon-search"></i></span><input type="text" class="input-medium" name="s" id="search" placeholder="<?php _e("Texto o título","bonestheme"); ?>" value="<?php the_search_query(); ?>" />		
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
+
+
+			<div class="row-fluid">
+				<h3>Etiquetas</h3>
+				<ul id="archives-tags">
+					<?php 
+						$tags = get_tags();
+						foreach ($tags as $tag) {
+							echo '<li><a href="' . get_tag_link($tag->term_id) . '" title="Posts sobre ' . $tag->name . '">' . $tag->name . '</a></li>' ;
+						}
+					?>
+				</ul>
+			</div>
 		</div>
 
+
 	</div>
+
 
 </div>
 <?php get_footer(); ?>
