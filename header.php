edbe7660793fa7cd7f5276d75d3b03f7b1b47fe3
Juan M. Hernández <juan.hernandez.arroyo@gmail.com>
2013-06-30 10:35:39 +0200

    fixed minor issue with viewport meta declaration

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 		
 		<title><?php wp_title('', true, 'right'); ?></title>
 				
-		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		
 		<!-- For everything else -->
 		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
